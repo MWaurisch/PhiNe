a49e8b06173bf5d22b96e0d00b6ad1e39ae58d62
Markus Waurisch <Markus.Waurisch@gmx.de>
2013-03-04 15:20:45 +0100

    first files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhiNe::Application.config.secret_token = '4e4161601840a7395ab6ab67867cfc6c646db2d3192246b751de9b40910c0b61911edaed378ea4391ee42afac2ae14b37b96e4781f35327667e3ab332c1772cd'
