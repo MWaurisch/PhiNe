970e3228be56cbf1b71ce3e2cd0e74cdd22e88a5
Matthias Nannt <matthias@nannt-online.de>
2013-03-05 12:47:08 +0100

    simple welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 PhiNe::Application.routes.draw do
+  get "welcome/index"
+
   resources :members
 
 
@@ -57,7 +59,7 @@ PhiNe::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
- root :to => 'questions#index'
+ root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
